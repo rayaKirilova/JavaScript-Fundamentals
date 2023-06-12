e5e652b568984ed443bf140f7710a205e52a197b
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2023-06-12 17:04:13 +0300

    Delete 06. Nested Loops directory

--- 06. Nested Loops/02. Multiplication Table.js
+++
@@ -1,9 +0,0 @@
-function multiplicationTable() {
-    for (let i = 1; i <= 10; i++) {
-        for (let y = 1; y <= 10; y++) {
-            let product = i * y;
-            console.log(`${i} * ${y} = ${product}`);
-        }
-    }
-}
-multiplicationTable(); 
